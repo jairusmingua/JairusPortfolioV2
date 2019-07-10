edd4f397dc89198b52c25eb2e16a3731c1313b9a
Jairus <minguajairus@yahoo.com.ph>
2019-07-11 00:40:08 +0800

    loading screen added

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,18 @@
 import  jump  from './jump.module.js';
-
-	
+const player = document.querySelector('lottie-player');
+$( document ).ready(function() {
+	loadingDone();
+});
 document.querySelector("#scrolltoinfo").addEventListener("click",()=>{
 	scrolltoInfo();
 })
 	function scrolltoInfo(){
 		jump("#about");
 	}
+
+function loadingDone(){
+	console.log("done");
+	document.querySelector(".loading").classList.toggle("done");
+	document.querySelector("lottie-player").classList.toggle("done");
+	player.stop();
+}
